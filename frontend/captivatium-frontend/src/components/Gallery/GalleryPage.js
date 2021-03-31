6f26698c1e70fc652b399b879550fa70c5d367f7
AbelHristodor <habelor@gmail.com>
2021-03-31 18:41:25 +0200

    added flexbox gallery

--- frontend/captivatium-frontend/src/components/Gallery/GalleryPage.js
+++ frontend/captivatium-frontend/src/components/Gallery/GalleryPage.js
@@ -3,10 +3,11 @@ import axios from 'axios';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
-import { PhotoSwipeGallery } from 'react-photoswipe';
+import { Gallery, Item } from 'react-photoswipe-gallery'
 
+import 'photoswipe/dist/photoswipe.css'
+import 'photoswipe/dist/default-skin/default-skin.css'
 import './Gallery.scss';
-import './photoswipe.scss';
 
 export default function GalleryPage() {
     const [urls, setUrls] = useState([]);
@@ -24,12 +25,6 @@ export default function GalleryPage() {
             });
     }, [])
 
-    const getThumbnailContent = (item) => {
-        return (
-          <img src={item.thumbnail} width={200} height="auto" alt={item.name} loading="lazy" />
-        );
-      }
-
     return (
         <Container fluid className="gallery-wrapper">
             {/*
@@ -39,17 +34,41 @@ export default function GalleryPage() {
             <Row className="categories-wrapper mt-2">
                 <Col sm="12" className="">
                     <ul className="categories list-group list-group-horizontal d-flex justify-content-center">
-                        <li className="category-item list-group-item"><a href="#asd">All</a></li>
-                        <li className="category-item list-group-item"><a href="#das">Landscapes</a></li>
-                        <li className="category-item list-group-item"><a href="#dd">Portraits</a></li>
+                        <li className="category-item list-group-item" data-filter="all"><a href="#asd">All</a></li>
+                        <li className="category-item list-group-item" data-filter="1"><a href="#das">Landscapes</a></li>
+                        <li className="category-item list-group-item" data-filter="2"><a href="#dd">Portraits</a></li>
                     </ul>
                 </Col>
             </Row>
             <Row className="gallery">
-                <Col sm="12">
-                    <PhotoSwipeGallery items={urls} thumbnailContent={getThumbnailContent} />
+                <Col sm="12 flex-gallery">
+                    { PhotoGallery(urls) }
                 </Col>
             </Row>
         </Container>
+    )
+}
+
+
+const PhotoGallery = (items) => {
+    return (
+        <Gallery>
+            { items.map((item) => {
+                return (
+                    <Item
+                        original={item.src}
+                        thumbnail={item.thumbnail}
+                        width={item.w}
+                        height={item.h}
+                    >
+                        {({ ref, open }) => (
+                            <div className="filtr-item">
+                                <img ref={ref} onClick={open} src={item.thumbnail} alt={item.title} />
+                            </div>
+                        )}
+                    </Item>
+                )
+            })}
+        </Gallery>
     )
 }

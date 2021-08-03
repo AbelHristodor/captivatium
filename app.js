142d9b16263892e14343ad1e3c41003263f942a6
AbelHristodor <habelor@gmail.com>
2021-08-03 12:38:50 +0200

    .

--- app.js
+++ app.js
@@ -71,10 +71,9 @@ app.use((error, req, res, next) => {
 
 if (process.env.NODE_ENV === 'production') {
     // Set the static assets folder (ie, client build)
-    logger.info("Here");
-    app.use(express.static('/client/build'));
+    app.use(express.static('./client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
     });
 }
 

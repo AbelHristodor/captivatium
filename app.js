2c291f81be5f36cf8ff7b8a98ba70749546f6a4c
AbelHristodor <habelor@gmail.com>
2021-08-03 12:32:12 +0200

    test

--- app.js
+++ app.js
@@ -71,6 +71,7 @@ app.use((error, req, res, next) => {
 
 if (process.env.NODE_ENV === 'production') {
     // Set the static assets folder (ie, client build)
+    logger.info("Here");
     app.use(express.static('/client/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));

6f2bd3d7c21cd8f2a36cd3a461da206428aa0bf9
AbelHristodor <habelor@gmail.com>
2021-08-04 10:39:03 +0200

    prepared for deployment

--- app.js
+++ app.js
@@ -68,12 +68,11 @@ app.use((error, req, res, next) => {
         }
     });
 });
-
 if (process.env.NODE_ENV === 'production') {
     // Set the static assets folder (ie, client build)
     app.use(express.static('./client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 }
 

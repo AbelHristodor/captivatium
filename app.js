7fe25bd2d8fa9b7398c1a482bfd66042b4f2db56
AbelHristodor <habelor@gmail.com>
2021-08-29 12:11:48 +0200

    changed folder structure

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(helmet.contentSecurityPolicy({
 }));
 app.use(cors(corsOptions));
 app.use(cookieParser());
-app.use(express.static('../frontend/build'));
+app.use(express.static(path.join(__dirname, 'client/build', 'index.html')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -75,7 +75,7 @@ app.use((error, req, res, next) => {
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 // Starting Server

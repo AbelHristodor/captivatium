507d972e6534d244a3e408d4c760f5309740a96d
AbelHristodor <habelor@gmail.com>
2021-08-29 10:05:23 +0200

    added csp

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ app.use(helmet.contentSecurityPolicy({
     directives: {
         'default-src': ["'self'", 'unsafe-inline'],
         'script-src': ["'self'", 'unsafe-inline'],
-        'image-src': ["'self'", 'unsafe-inline', '*'],
+        'img-src': ["'self'", 'unsafe-inline', '*'],
         'script-src-elem': ["'self'", 'unsafe-inline', '*']
     }
 }));
